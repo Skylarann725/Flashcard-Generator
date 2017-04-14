05ec10e374bc5d8afc517de2ff71280ae7d1ffec
Skylar Siagel <skylarsiagel@Skylars-MacBook-Pro.local>
2017-04-14 10:52:13 -0400

    added more to cloze

--- cloze.js
+++ cloze.js
@@ -1,6 +1,31 @@
 function ClozeCard(text, cloze) {
-	this.text = text;
-	this.cloze = cloze;
+    if (this instanceof ClozeCard) {
+
+        if (text.search(cloze) != -1) {
+
+            if (typeof text === "string") {
+                this.fullText = text;
+            } else {
+                console.log('Please use a string');
+
+            }
+            if (typeof cloze === "string") {
+                this.cloze = cloze;
+            } else {
+                console.log('Please use a string');
+            }
+
+            this.partialText = function() {
+                return this.fullText.replace(cloze, '...';)
+            };
+
+        } else {
+            throw new Error("Cloze statements must appear in full text.")
+        }
+    } else {
+        return new ClozeCard(text, cloze);
+    }
+
 }
 
 
