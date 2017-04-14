73201e46f487639ce56a5aaa474f8f4766d72bb6
Skylar Siagel <skylarsiagel@Skylars-MacBook-Pro.local>
2017-04-14 10:16:41 -0400

    added gitignore

---
+++ questions.js
@@ -0,0 +1,7 @@
+var inquirer = require('inquirer'); // for inquirer package
+
+var fs = require('fs'); // for fs
+
+var BasicCard = require('./basic.js'); // to retrieve basic card info
+
+var ClozeCard = require('./cloze.js'); // to retrieve close card info
