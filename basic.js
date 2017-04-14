7585b75150c1680b92379c784a40febb6a63c49f
Skylar Siagel <skylarsiagel@Skylars-MacBook-Pro.local>
2017-04-14 10:38:49 -0400

    added more code

--- basic.js
+++ basic.js
@@ -1,6 +1,16 @@
 function BasicCard(front, back) {
-	this.front = front;
-	this.back = back;
+    if (typeof front === "string") {
+        this.front = front;
+    } else {
+        console.log('Please use a string');
+
+    }
+    if (typeof back === "string") {
+        this.back = back;
+    } else {
+        console.log('Please use a string');
+    }
+
 }
 
 module.exports = BasicCard;
