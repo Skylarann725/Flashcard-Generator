81d9c505dedd963ce6d2744ac9d0ca6ea2f207a4
Skylar Siagel <skylarsiagel@Skylars-MacBook-Pro.local>
2017-04-18 15:00:02 -0400

    added comments

--- questions.js
+++ questions.js
@@ -13,61 +13,68 @@ function start() {
             name: 'action'
         }).then(function(answer) {
             switch (answer.action) {
+            	// if player picks basic card game
                 case 'Basic Card Game':
                     function playGameBasic() {
-                        var basicData = require('./BASIC.JSON');
+                        var basicData = require('./BASIC.JSON'); // calls basic.json file
                         var cards = []; // empty card array
                         var index = 0;
                         var score = 0;
 
+                        // loop to go through all the questions in the basic json file
                         for (var i = 0; i < basicData.length; i++) {
                             var currentCard = new BasicCard(basicData[i].front, basicData[i].back);
                             currentCard.push(cards);
-                            console.log(cards);
                         }
 
+                        // calls basic questions function
                         basicQuestions(cards, index, score);
                     }
 
                     function basicQuestions(cards, index, score) {
                         if (index < cards.length) {
-
                             var card = JSON.parse(cards[index]);
-                            console.log(card);
+                            // prompt to show the question and let the user input their answer
                             inquirer.prompt([{
                                 type: 'input',
                                 name: 'text',
                                 message: card.front + "\nAnswer: "
                             }]).then(function(answer) {
+                            	// if user answer equals the card answer then the user's score goes up
                                 if (answer.text === card.back) {
                                     score++;
                                 } else {
+                                	// if the user answer does not equal the card answer then show the below message and show them the correct answer
                                     console.log("Incorrect. The correct answer is " + card.back);
                                 }
+                                // goes to the next question
                                 index++;
                                 basicQuestions(cards, index, score);
                             });
 
                         } else {
+                        	// show's user their score when the game is finished and restarts the game
                             console.log('Your score is ' + score);
                             start();
                         }
                     }
                     break;
 
+                // if player picks cloze card game    
                 case 'Cloze Card Game':
                     function playGameCloze(cards, index, score) {
-                        var clozeData = require('./partial.json');
+                        var clozeData = require('./partial.json'); // calls partial.json file
                         var cards = []; // empty card array
                         var index = 0;
                         var score = 0;
 
+                        // loop to go through all the questions in the cloze json file
                         for (var i = 0; i < clozeData.length; i++) {
                             var currentCard = new ClozeCard(clozeData[i].front, clozeData[i].back);
                             currentCard.push(cards);
-                            console.log(cards);
                         }
 
+                        // calls cloze questions function
                         clozeQuestions(cards, index, score);
 
                     }
@@ -75,30 +82,35 @@ function start() {
                     function clozeQuestions(cards, index, score) {
                         if (index < cards.length) {
                             var card = cards[index];
-                            console.log(card);
+
+                            // prompt to show the question and let the user input their answer
                             inquirer.prompt([{
                                 type: 'input',
                                 name: 'text',
                                 message: card.front + "\nAnswer: "
                             }]).then(function(answer) {
+                            	// if user answer equals the card answer then the user's score goes up
                                 if (answer.text === card.back) {
                                     score++;
                                 } else {
+                                	// if the user answer does not equal the card answer then show the below message and show them the correct answer
                                     console.log("Incorrect. The correct answer is " + card.back);
                                 }
+                                // goes to the next question
                                 index++;
                                 clozeQuestions(cards, index, score);
                             });
                         } else {
+                        	// show's user their score when the game is finished and restarts the game
                             console.log('Your score is ' + score);
                             start();
                         }
-                    } // end of clozeQuestions
+                    } // end of clozeQuestions function
                     break;
 
             } // end of switch function
-        }) // end of first inquirer prompt
+        }) ;// end of first inquirer prompt
 
-} // end to start game function
+}; // end to start game function
 
 start();
